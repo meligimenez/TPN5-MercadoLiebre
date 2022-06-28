9b42029e5b0a91298c2a7244e9e3235f1010f7bd
Meli Gimenez <melinagimenez1999@gmail.com>
2022-06-28 15:38:05 -0300

    Creando el home

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
+
+app.listen(port, ()=> console.log(`Server running in http://localhost:${port}`));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', '/index.html')));
