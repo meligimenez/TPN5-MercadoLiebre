bdd02603471700cfc7919c4e7e8a8abc540053b3
Meli Gimenez <melinagimenez1999@gmail.com>
2022-06-30 22:53:03 -0300

    add iconos y estilo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3030;
+const port = 3000;
 
 app.listen(port, ()=> console.log(`Server running in http://localhost:${port}`));
 
