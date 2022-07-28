92f91a9a020b2a7227b1021e317cca87d6706e27
Meli Gimenez <melinagimenez1999@gmail.com>
2022-07-28 13:11:15 -0300

    add view register.html

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.listen(port, ()=> console.log(`Server running in http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running in http://localhost:${port}`));
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', '/index.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', '/register.html')));
