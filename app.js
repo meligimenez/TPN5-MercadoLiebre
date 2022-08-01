3663aae13f74b6ace018cb7455ba9f9752fdd07e
Meli Gimenez <melinagimenez1999@gmail.com>
2022-07-31 22:12:49 -0300

    add style register

--- app.js
+++ app.js
@@ -9,3 +9,4 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', '/index.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', '/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', '/login.html')));
